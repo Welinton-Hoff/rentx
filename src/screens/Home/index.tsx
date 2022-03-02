ad1a326d2e57b7218ec63736d8918b44f188182e
welinton <welinton.h.r@hotmail.com>
2022-03-02 08:40:59 -0300

    assest added and header create

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { StatusBar } from "react-native";
 
-import { Container } from "./styles";
+import {
+  Container,
+  Header,
+  RentxLogo,
+  TotalCars,
+  HeaderContent,
+} from "./styles";
 
 export function Home() {
-  return <Container></Container>;
+  return (
+    <Container>
+      <StatusBar
+        translucent
+        barStyle="light-content"
+        backgroundColor="transparent"
+      />
+
+      <Header>
+        <HeaderContent>
+          <RentxLogo />
+          <TotalCars>Total de 12 carros</TotalCars>
+        </HeaderContent>
+      </Header>
+    </Container>
+  );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,9 +1,38 @@
+import { RFValue } from "react-native-responsive-fontsize";
 import styled from "styled-components/native";
+
+import Logo from "../../assets/logo.svg";
 
 export const Container = styled.View`
   flex: 1;
-  align-items: center;
-  justify-content: center;
 
   background-color: ${({ theme }) => theme.colors.background_primary};
+`;
+
+export const Header = styled.View`
+  width: 100%;
+  height: 113px;
+
+  justify-content: flex-end;
+  padding: ${RFValue(32)}px ${RFValue(24)}px;
+
+  background-color: ${({ theme }) => theme.colors.header};
+`;
+
+export const HeaderContent = styled.View`
+  width: 100%;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const RentxLogo = styled(Logo)`
+  width: ${RFValue(108)}px;
+  height: ${RFValue(12)}px;
+`;
+
+export const TotalCars = styled.Text`
+  font-size: ${RFValue(15)}px;
+  color: ${({ theme }) => theme.colors.text};
+  font-family: ${({ theme }) => theme.fonts.primary_400};
 `;

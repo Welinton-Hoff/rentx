7e6e5f7bb5d7435cecb11c8d3a221c4eb4aeaad5
welinton <welinton.h.r@hotmail.com>
2022-03-02 08:03:22 -0300

    Defined global style

--- App.tsx
+++ App.tsx
@@ -1,7 +1,37 @@
 import React from "react";
+import AppLoading from "expo-app-loading";
+import { ThemeProvider } from "styled-components";
 
+import {
+  useFonts,
+  Inter_500Medium,
+  Inter_400Regular,
+} from "@expo-google-fonts/inter";
+import {
+  Archivo_500Medium,
+  Archivo_400Regular,
+  Archivo_600SemiBold,
+} from "@expo-google-fonts/archivo";
+
+import theme from "./src/styles/theme";
 import { Home } from "./src/screens/Home";
 
 export default function App() {
-  return <Home />;
+  const [fontsLoaded] = useFonts({
+    Inter_500Medium,
+    Inter_400Regular,
+    Archivo_500Medium,
+    Archivo_400Regular,
+    Archivo_600SemiBold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
+  );
 }

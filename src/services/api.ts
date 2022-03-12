9e97d7894ffd261e02ad0b4f60a8261129a961fe
welinton <welinton.h.r@hotmail.com>
2022-03-12 10:42:01 -0300

    config json-server to windows

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://192.168.1.34:3333",
+  // baseURL: "http://192.168.1.34:3333", // Linux
+  baseURL: "http://192.168.1.107:3333", // Windows
 });
 
 export default api;

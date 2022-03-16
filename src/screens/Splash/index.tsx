63124f0134e98d45a0d4587c921acf92504f9da1
welinton <welinton.h.r@hotmail.com>
2022-03-16 20:57:13 -0300

    splashScreen animated

--- src/screens/Splash/index.tsx
+++ src/screens/Splash/index.tsx
@@ -1,8 +1,72 @@
-import React from "react";
-import Animated from "react-native-reanimated";
+import { useNavigation } from "@react-navigation/native";
+import React, { useEffect } from "react";
+import Animated, {
+  runOnJS,
+  withTiming,
+  Extrapolate,
+  interpolate,
+  useSharedValue,
+  useAnimatedStyle,
+} from "react-native-reanimated";
 
-import { Container } from "./styles";
+import { Container, CustonLogo, CustonBrandSvg } from "./styles";
 
 export function Splash() {
-  return <Container></Container>;
+  const navigation = useNavigation();
+  const splashAnimation = useSharedValue(0);
+
+  const brandStyle = useAnimatedStyle(() => {
+    return {
+      opacity: interpolate(splashAnimation.value, [0, 50], [1, 0]),
+      transform: [
+        {
+          translateX: interpolate(
+            splashAnimation.value,
+            [0, 50],
+            [0, -50],
+            Extrapolate.CLAMP
+          ),
+        },
+      ],
+    };
+  });
+
+  const logoStyle = useAnimatedStyle(() => {
+    return {
+      opacity: interpolate(splashAnimation.value, [0, 25, 50], [0, 0.3, 1]),
+      transform: [
+        {
+          translateX: interpolate(
+            splashAnimation.value,
+            [0, 50],
+            [-50, 0],
+            Extrapolate.CLAMP
+          ),
+        },
+      ],
+    };
+  });
+
+  const initApp = () => {
+    navigation.navigate("Home");
+  };
+
+  useEffect(() => {
+    splashAnimation.value = withTiming(50, { duration: 2500 }, () => {
+      "worklet";
+      runOnJS(initApp)();
+    });
+  }, []);
+
+  return (
+    <Container>
+      <Animated.View style={[brandStyle, { position: "absolute" }]}>
+        <CustonBrandSvg />
+      </Animated.View>
+
+      <Animated.View style={[logoStyle, { position: "absolute" }]}>
+        <CustonLogo />
+      </Animated.View>
+    </Container>
+  );
 }
--- src/screens/Splash/styles.ts
+++ src/screens/Splash/styles.ts
@@ -1,8 +1,21 @@
 import styled from "styled-components/native";
 
+import LogoSvg from "../../assets/logo.svg";
+import BrandSvg from "../../assets/brand.svg";
+
 export const Container = styled.View`
   flex: 1;
   align-items: center;
   justify-content: center;
   background-color: ${({ theme }) => theme.colors.header};
 `;
+
+export const CustonLogo = styled(LogoSvg).attrs({
+  width: 180,
+  height: 20,
+})``;
+
+export const CustonBrandSvg = styled(BrandSvg).attrs({
+  width: 80,
+  height: 50,
+})``;

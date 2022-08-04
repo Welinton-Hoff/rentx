ff1d4736219fbcf0422f23a53657d74af9d0f035
welinton <welinton.h.r@hotmail.com>
2022-08-04 01:43:02 +0100

    remane SignIn screen

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -3,9 +3,10 @@ import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import { Home } from "../screens/Home";
-import { SignIn } from "../screens/SignIn";
+
 import { Splash } from "../screens/Splash";
 import { MyCars } from "../screens/MyCars";
+import { SignIn } from "../screens/SignIn";
 import { CarDetails } from "../screens/CarDetails";
 import { Scheduling } from "../screens/Scheduling";
 import { InitialData } from "../screens/signUp/InitialData";

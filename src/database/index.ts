ca8456de1be469882a51722a00b82abd122d8119
welinton <welinton.h.r@hotmail.com>
2022-10-15 17:52:53 -0300

    Car table created in local database

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,6 +2,8 @@ import { Database } from "@nozbe/watermelondb";
 import SQLiteAdapter from "@nozbe/watermelondb/adapters/sqlite";
 
 import { schemas } from "./schema";
+
+import { Car } from "./model/Car";
 import { User } from "./model/User";
 
 const adapter = new SQLiteAdapter({
@@ -10,5 +12,5 @@ const adapter = new SQLiteAdapter({
 
 export const database = new Database({
   adapter,
-  modelClasses: [User],
+  modelClasses: [User, Car],
 });
